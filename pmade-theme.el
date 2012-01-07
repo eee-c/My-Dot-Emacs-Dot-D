c83737aa533eb16ee1b08eac0b623d7b47dfcee7
Chris Strom <eee.c@eeecooks.com>
2012-01-06 22:35:20 -0500

    Dart mode and pmade color scheme

---
+++ pmade-theme.el
@@ -0,0 +1,8 @@
+(deftheme pmade
+  "Created 2011-05-11.")
+
+(custom-theme-set-faces
+ 'pmade
+ )
+
+(provide-theme 'pmade)
---
+++ pmade2-theme.el
@@ -0,0 +1,8 @@
+(deftheme pmade2
+  "Created 2011-05-11.")
+
+(custom-theme-set-faces
+ 'pmade2
+ )
+
+(provide-theme 'pmade2)
